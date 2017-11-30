daeb37020b861b35b6b6e380f87d40120bf9fa06
akshay kulkarni <kumar.akshay.kulkarni@gmail.com>
2017-11-30 17:19:14 -0600

    added getquestioncommentdict

--- SemEval2017Task3/Preprocess.py
+++ SemEval2017Task3/Preprocess.py
@@ -7,16 +7,27 @@ from bs4 import BeautifulSoup as bs
 
 class PreProcess:
 
-    def __init__(self,path):
-        self.path = path
+    def __init__(self):
+        return
 
-    def GetQuestionCommentDict():
+    def GetQuestionCommentDict(self,path):
         """  return dict of this format
-             key : ('Q268','question text') : [ ('Q268_R4_C1' , 'comment text') , ....]
+             key : 'question text' : [ ('Q268_R4_C1' , 'comment text') , ....]
         """
-        pass
+        qc = {}
+        soup = None
+        with open(path,errors='ignore') as inp:
+            soup = bs(inp,'lxml-xml')
+        questions = soup.find_all('RelQuestion')
+        comments = soup.find_all('RelComment')
+        for question in questions:
+            relevant_comments = [ (z['RELC_ID'],z.text.rstrip().strip('\n')) for z in comments 
+                                 if question['RELQ_ID'] in z['RELC_ID']]
+            qc[question.text.rstrip().strip('\n')] = relevant_comments
+        return qc
 
-    def GetTruthTable():
+
+    def GetTruthTable(self,path):
         """
            From relevancy file build this table
            key : 'Q268_R4_C1' : true/false
--- SemEval2017Task3/classifier.py
+++ SemEval2017Task3/classifier.py
@@ -1,5 +1,5 @@
 import argparse
-import Preprocess
+from Preprocess import PreProcess
 import BuildRep
 import pandas as pd 
 import numpy as np 
@@ -16,15 +16,19 @@ USAGE: classifier.py --train <path> --test <path> --mode <rep or classify> --out
 """
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='USAGE: classifier.py --train <path> --test <path> --mode <rep or classify>')
+	parser = argparse.ArgumentParser(description='USAGE: classifier.py --train <path> --test <path> --mode <rep or classify>')
 	parser.add_argument('-tr','--train',required=True)
-    parser.add_argument('-te','--test',required=True)
-    parser.add_argument('-m','--mode',required=True)
-    parser.add_argument('-o','--out',required=False)
+	parser.add_argument('-te','--test',required=True)
+	parser.add_argument('-m','--mode',required=True)
+	parser.add_argument('-o','--out',required=False)
 	args= vars(parser.parse_args())
 
-    if args['mode'] == 'rep':
-        pass
-    elif args['mode'] == 'classify':
-        pass
-    # will 
+	if args['mode'] == 'rep':
+		preprocessor = PreProcess()
+		question_comments = preprocessor.GetQuestionCommentDict(args['train'])
+		for k in question_comments.keys():
+			print(k,' : ',question_comments[k])
+		print(len(question_comments))
+	elif args['mode'] == 'classify':
+		pass
+	# will 
