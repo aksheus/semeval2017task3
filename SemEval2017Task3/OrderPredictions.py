2b407ad87121c1ede0f5d6c1ade55de2fc305c5a
akshay kulkarni <kumar.akshay.kulkarni@gmail.com>
2017-12-03 22:58:44 -0600

    added order preds

---
+++ SemEval2017Task3/OrderPredictions.py
@@ -0,0 +1,41 @@
+import functools
+import sys
+import os
+
+"""
+     USAGE: python OrderPredicitons path/input.predictions  path/output.predictions
+"""
+
+def compare(item1,item2):
+    if item1[0] == item2[0]:
+        x = int(item1[2])
+        y = int(item2[2])
+        if x<y:
+           return -1
+        elif x==y:
+            return 0
+        else:
+            return 1
+    else:
+        if item1[0] > item2[0]:
+            return 1
+        else:
+            return -1
+
+def order_predictions(content):
+    return sorted(content,key=functools.cmp_to_key(compare))
+
+if __name__ == '__main__':
+    if os.path.isfile(sys.argv[1]):
+        content = []
+        with open(sys.argv[1]) as inp:
+            for line in inp:
+                content.append(line)
+        content = [ tuple(z.split()) for z in content]
+        proper_content = order_predictions(content)
+        with open(sys.argv[2],'w',encoding='utf-8') as out:
+            for column in proper_content:
+                out.write('\t'.join(col for col in column))
+                out.write('\n')
+
+
